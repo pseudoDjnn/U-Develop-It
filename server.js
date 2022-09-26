cf94696ab908e24631a75dfcb4ba434f1273b471
p5eudoDjnn <alexander.havers@gmail.com>
2022-09-26 15:37:13 -0400

    added catch all and correct path for SQL

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // IMPORTS
 const express = require("express");
+const mysql = require("mysql2");
 
 // PORT
 const PORT = process.env.PORT || 3001;
@@ -10,10 +11,27 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // GET ROUTE
-app.get("/", (req, res) => {
-  res.json({
-    message: "Hello World",
-  });
+// app.get("/", (req, res) => {
+//   res.json({
+//     message: "Hello World",
+//   });
+// });
+
+// Connect to the database
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    // Your MySQL username,
+    user: "lavahrexes",
+    // Your passowrd
+    password: "!NaPHtHYloxy07131988",
+    database: "election",
+  },
+  console.log("Connected to the election database.")
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
 });
 
 // Default response for any other request (Not Found)
